f67e5a634825108e0f3078d0504fa6a88dce0f0a
Taufik Amaryansyah <taufik.amary@gmail.com>
2018-10-09 12:26:55 +0700

    hal19

---
+++ std pert3 hal19/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <conio.h>
+#include <string>
+#include <stdlib.h>
+
+using namespace std;
+
+struct movies_t{
+	char title [50];
+	int year;
+}mine, yours;
+
+void printmovie(movies_t movie);
+
+int main(){
+	char buffer[50];
+
+	strcpy(mine.title, "agung di tinggal nikah");
+	mine.year = 2003;
+
+	cout<<"Masukan judul film favorit: ";
+	cin.getline(yours.title,50);
+	cout<<"Masukan tahun: ";
+	cin.getline(buffer,50);
+	yours.year=atoi(buffer);
+
+	cout<<"Judul film favorite yang ada:\n ";
+	printmovie(mine);
+	cout<<"Judul film favorite kamu adalah:\n ";
+	printmovie(yours);
+	getch();
+}
+
+void printmovie(movies_t movie)
+{
+	cout<<movie.title;
+	cout<<" ("<<movie.year<<")\n";
+}
+
